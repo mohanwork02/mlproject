dec84c8e30659a69ae516f5f847043d31d658e50
mohanwork02 <mohanwork02@gmail.com>
2025-11-01 14:27:52 +0530

    model training and hyperparamenter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,9 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            #df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv(r"D:\mlproject\notebook\data\stud.csv")
+            
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
